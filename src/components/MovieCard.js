f04a4d98f79a51fe2b0367133f01ddf3d3a1211a
John Doe <shamailov@gmail.com>
2021-04-25 02:28:30 +0300

    added multiple api calls to get all data

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,13 +4,14 @@ import { Row } from 'react-bootstrap';
 import './MovieCard.css'
 
 function MovieCard({ movie }) {
+    //if (!movie) return '';
     return (
         <div className="c-movie">
             <Card bg="secondary" text='white'>
                 <div className="card-horizontal">
                 {movie.poster?<Card.Img variant="top" src={movie.poster} />:null}
                 <Card.Body>
-                    <Card.Title>{`${movie.name} (${movie.release_year()})  ${movie.id}`}</Card.Title>
+                    <Card.Title>{`${movie.name} (${movie.release_year()}) `}</Card.Title>
                     <ListGroup variant="flush">
                         <ListGroup.Item variant="secondary">
                                 <span className="card-label">Length:</span>{movie.length+" minutes"}
@@ -18,7 +19,7 @@ function MovieCard({ movie }) {
                         <ListGroup.Item variant="secondary">
                             <span className="card-label">Director:</span>{movie.director}</ListGroup.Item>
                         <ListGroup.Item variant="secondary">
-                            <span className="card-label">Main stars:</span>{movie.stars.join(", ")}</ListGroup.Item>
+                            <span className="card-label">Main stars:</span>{movie.stars}</ListGroup.Item>
                         <ListGroup.Item variant="secondary">
                             <span className="card-label">Overview:</span>
                             <span>{movie.plot}</span>
--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -6,38 +6,76 @@ import configData from "../../package.json";//to hide TMDB API keys
 import MovieCard from '../components/MovieCard.js';
 import MoviesModel from '../model/MovieModel.js';
 
-function MoviePage({actorId} ) {
-
+function MoviePage({actorId}) {
     const api_key = configData.tmdb_api_key //get TMDB API key from config file
-
-    const [results, setResults] = useState([]);
     const [movies, setMovies] = useState([])
 
     //`movie id https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&&with_cast=${actorId}&sort_by=vote_average.desc`
     // runtime `https://api.themoviedb.org/3/movie/277129?api_key=53d2ee2137cf3228aefae083c8158855&language=en-US`
     // stars `https://api.themoviedb.org/3/movie/684426/credits?api_key=53d2ee2137cf3228aefae083c8158855&language=en-US`
     // videos `https://api.themoviedb.org/3/movie/157336/videos?api_key=53d2ee2137cf3228aefae083c8158855`
-    
+      
+    useEffect(() => {
 
+            const getMovies = async ()=> {
+                try {
+                    const moviesData1 = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_cast=${actorId}&sort_by=vote_average.desc`);
+                    const moviesData2 = await Promise.all(moviesData1.data.results.map(({ id }) => {
+                        //return axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${api_key}&language=en-US`);
+                        return axios.all([
+                            axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${api_key}&language=en-US`),
+                            axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}&language=en-US`)
+                          ])
+                    }));
+                    const allData = await Promise.all([moviesData1, moviesData2]);                
+
+                    const moviesId = allData[0].data.results
+                    const moviesData = allData[1]
+
+                    console.log(moviesData[0][1]);
+
+                    const setData = async () => {
+                        setMovies(moviesId.map((movie, index) => 
+                            new MoviesModel(
+                                movie.id,
+                                movie.release_date,
+                                movie.title,
+                                moviesData[index][1].data.runtime, 
+                                moviesData[index][0].data.crew.map(item =>                             
+                                    item.job==="Director"?
+                                      item.name : ''
+                                ),
+                                movie.poster_path , 
+                                movie.overview, 
+                                moviesData[index][0].data.cast.slice(0, 10).map(item =>                             
+                                    item.name+', '
+                                )
+                            )
+                        
+                        ))    
     
-    //get movies from TMDB api
-    useEffect(async () => {
-        const fetchData = async () => {
-            const result = await axios(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&&with_cast=${actorId}&sort_by=vote_average.desc`);
-            setResults(result.data.results);
-          };       
-          fetchData();
+                    }
+                    setData();
+
+                } catch (err) {
+                    console.log(err.stack);
+                }
+            };
+            getMovies();
+
+
     }, [actorId])
 
-    useEffect(() => {
-        setMovies(results.map(movie => new MoviesModel(movie.id, movie.release_date, movie.title, 125, "director",movie.poster_path , movie.overview, ["Star1", "Star2", "Star3", "Star4"])))    
-    }, [results])
+
 
     return (        
         <div className="p-movies">
             <Container>
                 <Row>
+                    
                     {movies.map((movie, index) => <Col key={"col"+index} lg={12}><MovieCard movie={movie}/></Col>)}
+                    
+                    
                 </Row>
             </Container>
         </div>
