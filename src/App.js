d540e7e009382b20396921a0505b77367c2648b9
John Doe <shamailov@gmail.com>
2021-04-25 13:15:24 +0300

    Added React toute-dom and new  pages for "home" and "not found"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import ActoresPage from './pages/ActoresPage';
 import React, { useState } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import ActoresPage from './pages/ActoresPage';
 import MoviePage from './pages/MoviePage';
+import HomePage from './pages/HomePage/HomePage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
 function App() {
 
   const [actorId, setActorId] = useState(null);
 
   return (
-    <div>
-      <ActoresPage getActorId={(actorId)=>{
-        setActorId(actorId)
-      }} />   
-      
-      {actorId? <MoviePage actorId={actorId} />:null}
-    </div>
+
+    <HashRouter>
+        <Switch>
+          <Route exact path="/"><HomePage/></Route>
+          <Route exact path="/actors"><ActoresPage getActorId={(actorId)=>{setActorId(actorId)}} />   </Route>
+          <Route exact path="/actores/:id/movie">{actorId? <MoviePage actorId={actorId} />:null}</Route>
+          <Route path="/"><NotFoundPage/></Route>
+        </Switch>
+    </HashRouter>
+
   );
 }
 
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
+function HomePage(props) {
+    return (
+        <div>
+            <Container>
+                <h1 className="display-1">Movies App</h1>
+                <p>Movies App!!</p>
+                <Link className="btn btn-primary" to="/actors">See all actors</Link>
+            </Container>
+        </div>
+    );
+}
+
+export default HomePage;
