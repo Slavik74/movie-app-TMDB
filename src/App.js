90e980af784e865e480cc829077608d653875983
John Doe <shamailov@gmail.com>
2021-04-27 23:19:12 +0300

    use google font for headline text

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import ActoresPage from './pages/ActoresPage/ActoresPage';
 import MoviePage from './pages/MoviePage/MoviePage';
